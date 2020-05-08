b95416855b4dcda8b4ac75004e43b4ed28ba5a79
loveleensharma <loveleensharma1321995@gmail.com>
2020-05-08 20:50:09 +0530

    added dealspage class

---
+++ src/main/java/com/qa/hubspot/pages/Dealspage.java
@@ -0,0 +1,12 @@
+package com.qa.hubspot.pages;
+
+public class Dealspage {
+	 public Dealspage() {
+		 System.out.println("deals construtor");
+	 }
+	 
+	 public void navigatedealspage() {
+		 System.out.println("clcik on deals page link");
+	 }
+
+}
